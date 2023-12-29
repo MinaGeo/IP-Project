4fe6466e8c63b0530787a65039a5fa3ed8b583fa
MinaGeo <20P4198@eng.asu.edu.eg>
2023-12-29 23:07:04 +0200

    Stores details added

--- Controllers/storesController.cs
+++ Controllers/storesController.cs
@@ -34,7 +34,13 @@ namespace WebApplication1.Controllers
             }
             return View(store);
         }
-
+        [HttpPost, ActionName("Details")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DetailsBack(int id)
+        {
+            product product = db.products.Find(id);
+            return RedirectToAction("Index");
+        }
         // GET: stores/Create
         public ActionResult Create()
         {
